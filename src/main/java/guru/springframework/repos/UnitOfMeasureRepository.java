66b8d888fe0398bc94035b7b8df9cc1d77112e11
Steindl Kristof <kristof.steindl@gmail.com>
2020-06-06 19:25:47 +0200

    140 added auto fill SQL (data.sql), and extended Repository methods

--- src/main/java/guru/springframework/repos/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repos/UnitOfMeasureRepository.java
@@ -1,7 +1,12 @@
 package guru.springframework.repos;
 
+import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
